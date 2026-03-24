e2ce9b0859c72ba312edb20475e95abda67035e7
sandeepit8048 <rkysandeepyadav@gmail.com>
2026-03-24 15:08:58 +0530

    add function on form

--- src/component/StudentForm.jsx
+++ src/component/StudentForm.jsx
@@ -1,44 +1,56 @@
 import React, { useState } from 'react'
 
 function StudentForm() {
-    const [form , setForm]= useState({
-        user:'',
-        email:'',
-        phone:'',
-        id:''
+    const [form, setForm] = useState({
+        user: '',
+        email: '',
+        phone: '',
+        id: ''
     })
 
-    function handleChange(e){
-        const [name,value]= e.target.value
+    function handleChange(e) {
+        const { name, value } = e.target
 
         setForm({
-            ...form, 
-             name:e.target.value
+            ...form,
+            [name]: value
         })
     }
 
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        setForm({
+            user: '',
+            email: '',
+            phone: '',
+            id: ''
+        })
+    }
+
+
     console.log(form);
 
-      
-
-  return (
-    <div>
-        <h1>Form</h1>
-        <form action="" >
-            <label htmlFor="">User-Name</label>
-            <input type="text" placeholder='user-name' id='' name='user' onChange={handleChange}  />
-             <label htmlFor="">E-mail</label>
-             <input type="email" name="email" id="" placeholder='xyz@gmail.com' onChange={handleChange} />
-             <label htmlFor="">Phone-Num</label>
-             <input type="number" name="phone" id="" placeholder='+91-9572720939' onChange={handleChange} />
-             <label htmlFor="">College-Id</label>
-             <input type="number" name="id" id="" placeholder='A2026it8048' onChange={handleChange} />
-             <button type='subit' >Submit</button>
-        </form>
 
 
-    </div>
-  )
+    return (
+        <div>
+            <h1>Form</h1>
+            <form action="" onSubmit={handleSubmit}>
+                <label htmlFor="">User-Name</label>
+                <input type="text" placeholder='user-name' id='' name='user' onChange={handleChange} value={form.user} />
+                <label htmlFor="">E-mail</label>
+                <input type="email" name="email" id="" placeholder='xyz@gmail.com' onChange={handleChange} value={form.email} />
+                <label htmlFor="">Phone-Num</label>
+                <input type="number" name="phone" id="" placeholder='+91-9572720939' onChange={handleChange} value={form.phone} />
+                <label htmlFor="">College-Id</label>
+                <input type="number" name="id" id="" placeholder='A2026it8048' onChange={handleChange} value={form.id} />
+                <button type='subit' >Submit</button>
+            </form>
+
+
+        </div>
+    )
 }
 
 export default StudentForm
